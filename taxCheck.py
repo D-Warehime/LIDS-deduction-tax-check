29cfead1caaed607d4ef349e7397519fc353f062
Daniel <dwarehime@kellyway.com>
2019-07-23 16:39:40 -0400

    small changes

--- taxCheck.py
+++ taxCheck.py
@@ -1,1 +1,21 @@
+import xlrd
+import xlwt
+import pymssql
+import sys
+import csv
+import math
+
+conn = pymssql.connect(
+	host='kaig_prodqa.db.kaiglan.com',
+	# port=1438,
+	database='KAIG',)
+
+file_path = '261931Deductions20190711.csv' 
+
+ErrorRecords = []
+
+with open(file_path) as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+	for row in readCSV:
+		print(row)
 
